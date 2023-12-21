b68309fb52945157181673598b1832c27e047170
yauhenAleo <152736554+YauhenAleo@users.noreply.github.com>
2023-12-21 06:41:52 +0100

    Update main.rs

--- cli/main.rs
+++ cli/main.rs
@@ -11,21 +11,4 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
-
-use aleo::{commands::CLI, helpers::Updater};
-use clap::Parser;
-
-fn main() -> anyhow::Result<()> {
-    // Parse the given arguments.
-    let cli = CLI::parse();
-    // Run the updater.
-    println!("{}", Updater::print_cli());
-    // Run the CLI.
-    match cli.command.parse() {
-        Ok(output) => println!("{output}\n"),
-        Err(error) => println!("⚠️  {error}\n"),
-    }
-    Ok(())
-}
+// 
