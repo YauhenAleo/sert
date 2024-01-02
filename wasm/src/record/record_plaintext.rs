db0b570ec171fbc5269b481652def4d65ad6baae
yauhenAleo <152736554+YauhenAleo@users.noreply.github.com>
2024-01-02 10:41:59 +0100

    Update record_plaintext.rs

--- wasm/src/record/record_plaintext.rs
+++ wasm/src/record/record_plaintext.rs
@@ -1,29 +1,7 @@
 // Copyright (C) 2019-2023 Aleo Systems Inc.
 // This file is part of the Aleo library.
 
-// The Aleo library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
-// The Aleo library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with the Aleo library. If not, see <https://www.gnu.org/licenses/>.
-
-use crate::{
-    account::PrivateKey,
-    types::{IdentifierNative, ProgramIDNative, RecordPlaintextNative},
-};
-
-use std::{ops::Deref, str::FromStr};
-use wasm_bindgen::prelude::*;
-
-/// Aleo record plaintext
-#[wasm_bindgen]
 #[derive(Clone)]
 pub struct RecordPlaintext(RecordPlaintextNative);
 
