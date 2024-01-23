ec29715d5982caddd48a4b4665471e72a3701c15
yauhenAleo <152736554+YauhenAleo@users.noreply.github.com>
2024-01-23 15:20:26 +0100

    Update kcov.sh

--- .resources/kcov.sh
+++ .resources/kcov.sh
@@ -5,3 +5,10 @@ echo "---END_LIST---"
 for file in target/debug/deps/aleo*-*[^\.d];
   do
   
+cd /home/circleci/project/ &&
+echo "---START_LIST---"
+ls target/debug/deps
+echo "---END_LIST---"
+for file in target/debug/deps/aleo*-*[^\.d];
+  do
+  
